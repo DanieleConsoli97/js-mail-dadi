1971ab5094899af483f46c0024b1b070cdb193d3
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-01-14 14:39:20 +0100

    end code

--- script.js
+++ script.js
@@ -11,8 +11,8 @@
 let mailInvitati = [];
 let numeroPartecipanti;
 let mail;
-let emailCheck;
-numeroPartecipanti = parseInt( prompt("Inserisci il numero dei partecipanti"))
+let emailCheck = false;
+numeroPartecipanti = parseInt(prompt("Inserisci il numero dei partecipanti"))
 for( i = 0; i < numeroPartecipanti; i++){
     mailInvitati [i] = prompt(`Inserisci la mail dell' invitato numero ${i+1}`)
 }
@@ -22,14 +22,12 @@ mail = prompt("Inserisci la tua mail")
 for ( i=0 ; i < mailInvitati.length; i++){
     if (mailInvitati[i] == mail){
         emailCheck=true;
-    }else{
-        emailCheck=false;
+        console.log("Puoi entrare alla festa");
+        break;
     }
 }
-if(emailCheck == true){
-    console.log("Puoi entrare alla festa");
-}else{
-    console.log("non puoi entrare alla festa")
+if(!emailCheck){
+    console.log("Non puoi entrare alla festa");
 }
 
 
@@ -45,3 +43,18 @@ if(emailCheck == true){
 // 3. si ma noi cosa vogliamo fare?
 // 4. torniamo a scrivere in italiano
 // 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+let risultatoDado = [];
+let vincitore;
+risultatoDado[0]= Math.round(Math.random() *6);
+risultatoDado[1]= Math.round(Math.random() *6);
+console.log(`"il dado del giocatore è" ${risultatoDado[0]}`);
+console.log(`"Il dado del computer è"${risultatoDado[0]}`);
+if (risultatoDado[0]>risultatoDado[1]){
+    console.log("il giocatore ha vinto ");
+} else if(risultatoDado[0]<risultatoDado[1]){
+    console.log("il computer ha vinto ");
+
+} else{
+    console.log("Il risultato è pari");
+}
